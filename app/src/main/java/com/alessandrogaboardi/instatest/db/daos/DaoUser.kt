1f5c9596cf331ce89f43b9715af1c836a09cf17b
Luigi Aiello <luigi-aiello@live.it>
2017-11-03 09:02:48 +0100

    Moved logic from activity to fragment

---
+++ app/src/main/java/com/alessandrogaboardi/instatest/db/daos/DaoUser.kt
@@ -0,0 +1,7 @@
+package com.alessandrogaboardi.instatest.db.daos
+
+/**
+ * Created by luigi on 03/11/17.
+ */
+class DaoUser {
+}
---
+++ app/src/main/java/com/alessandrogaboardi/instatest/db/models/ModelUser.kt
@@ -0,0 +1,7 @@
+package com.alessandrogaboardi.instatest.db.models
+
+/**
+ * Created by luigi on 03/11/17.
+ */
+class ModelUser {
+}
