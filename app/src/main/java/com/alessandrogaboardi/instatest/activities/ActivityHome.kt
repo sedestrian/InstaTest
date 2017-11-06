47b8c4b209c8c37f6809480290f55bdf9cdde139
Alessandro <alessandro.gaboardi@mindtek.it>
2017-11-06 03:27:13 +0100

    Switched to programmatic fragment and added callbacks
    Switched hardcoded fragment to programmatically added. Added profile and media detail callbacks

--- app/src/main/java/com/alessandrogaboardi/instatest/activities/ActivityHome.kt
+++ app/src/main/java/com/alessandrogaboardi/instatest/activities/ActivityHome.kt
@@ -1,45 +1,64 @@
 package com.alessandrogaboardi.instatest.activities
 
-import android.app.Activity
 import android.content.Intent
-import android.content.pm.ApplicationInfo
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityOptionsCompat
+import android.support.v4.app.SharedElementCallback
+import android.support.v4.util.Pair
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
 import com.alessandrogaboardi.instatest.R
-import com.alessandrogaboardi.instatest.db.daos.DaoToken
-import com.alessandrogaboardi.instatest.kotlin.extensions.getLoginIntent
-import com.alessandrogaboardi.instatest.ws.ApiManager
-import com.alessandrogaboardi.instatest.ws.callbacks.UserDataCallback
-
+import com.alessandrogaboardi.instatest.communicators.ActivityHomeCommunicator
+import com.alessandrogaboardi.instatest.db.models.ModelMedia
+import com.alessandrogaboardi.instatest.fragments.FragmentHome
+import com.alessandrogaboardi.instatest.kotlin.extensions.replaceMainFragment
+import com.alessandrogaboardi.instatest.kotlin.extensions.setGone
 import kotlinx.android.synthetic.main.activity_home.*
-import okhttp3.Call
-import okhttp3.Response
-import java.io.IOException
+import kotlinx.android.synthetic.main.gallery_item.view.*
 
-class ActivityHome : AppCompatActivity() {
-
+class ActivityHome : AppCompatActivity(), ActivityHomeCommunicator {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setSupportActionBar(toolbar)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_activity_home, menu)
-        return true
-    }
+        supportActionBar?.title = getString(R.string.app_name)
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        replaceMainFragment(FragmentHome.newInstance(), R.id.placeholder, false)
+
+        profileIcon.setOnClickListener {
+            openProfileActivity()
         }
+    }
+
+    private fun openProfileActivity() {
+        val intent = Intent(this, ActivityUserProfile::class.java)
+        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                this,
+                profileIcon,
+                getString(R.string.profile_icon_transition_name)
+        )
+
+        startActivity(intent, options.toBundle())
+    }
+
+
+    override fun onMediaDetailRequested(item: ModelMedia, view: View) {
+        var intent = Intent(this, ActivityMediaDetail::class.java)
+        intent.putExtra(ActivityMediaDetail.MEDIA_ID_EXTRA, item.id)
+
+        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                this,
+                Pair(view.picture, getString(R.string.transition_media_detail_image)),
+                Pair(view.liked, getString(R.string.transition_media_detail_liked_icon)),
+//                Pair(view.likes, getString(R.string.transition_media_detail_liked_text)),
+                Pair(view.commentsIcon, getString(R.string.transition_media_detail_comment_icon)),
+//                Pair(view.comments, getString(R.string.transition_media_detail_comment_text)),
+                Pair(view.userPicture, getString(R.string.transition_media_detail_user_picture)),
+                Pair(view.username, getString(R.string.transition_media_detail_username)),
+                Pair(view.description, getString(R.string.transition_media_detail_description))
+        )
+
+        startActivity(intent, options.toBundle())
     }
 }
