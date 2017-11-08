069b33f90ff07aef3ecb46ddaad4759f5581ed41
Alessandro <alessandro.gaboardi@mindtek.it>
2017-11-08 02:48:18 +0100

    Fixed login backstack

--- app/src/main/java/com/alessandrogaboardi/instatest/kotlin/extensions/ContextExtensions.kt
+++ app/src/main/java/com/alessandrogaboardi/instatest/kotlin/extensions/ContextExtensions.kt
@@ -8,5 +8,7 @@ import com.alessandrogaboardi.instatest.activities.ActivityLogin
  * Created by alessandrogaboardi on 02/11/2017.
  */
 fun Context.getLoginIntent(): Intent {
-    return Intent(this, ActivityLogin::class.java)
+    val intent =  Intent(this, ActivityLogin::class.java)
+    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+    return intent
 }
